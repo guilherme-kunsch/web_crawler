d0fed59a8a50158de25659bff5c8c8a2072ac6cf
guilherme-kunsch <guilherme.kunsch@outlook.com>
2024-10-10 20:10:08 -0300

    first commit

---
+++ teste.py
@@ -0,0 +1,3 @@
+import requests;
+
+url = "https://freterapido.com.br/?lp=1&utm_campaign=branded-search&utm_source=google&utm_medium=paidsearch&utm_term=frete%20r%C3%A1pido&adgroupid=158250831973&loc_physical_ms=9101053&gad_source=1&gclid=CjwKCAjw9p24BhB_EiwA8ID5BlRi3TMwglWHh-MBPT0Me4dcqELg5czYWZ2N98KT3-CijjXgcpZIKRoCtDQQAvD_BwE"
